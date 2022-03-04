5d872f4ea3121357a003dd742d1c6de6503d7b9b
Praneet S <96655249+PraneetDeveloper@users.noreply.github.com>
2022-03-04 16:45:46 +0530

    Frigate - 04-03-2022

---
+++ frigate/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import HomePage1 from './Components/Home Page 1/homePage1';
+
+function App() {
+  return (
+    <div>
+     <HomePage1 />
+    </div>
+  );
+}
+
+export default App;
